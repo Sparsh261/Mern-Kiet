03b267514386e361faec79180930719fedb43d05
Sparsh Chauhan <142965846+Sparsh261@users.noreply.github.com>
2024-04-05 09:29:28 +0530

    Uploaded Day 3 work

---
+++ Day_3/Javascript/script.js
@@ -0,0 +1,168 @@
+// Hoisting means first;y language reads the code and then executes later . It can be seen when variable with var keyword is to console first then declared.
+
+//  Js is loosely typed that is same variable can be assigned integer then string after some time. 
+
+// Var is oldest keyword, functional scope can be used to declare same variable twice.
+
+// let is block scope, it can't be redeclared. 
+
+// const variables can't be reassigned or redclared.
+
+// Js is inter[reted but mosstly just in time interpreted.
+
+// Priitive datatypes are immutable
+
+//  undefined vs Null
+
+// parseInt converts string to number until a non-numeric character arrives while Number converts if only non-numeric characters are present in string.
+
+
+
+
+// How many ways are there to return values from arrow functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(message);
+// let message = "Hello";      // output -- intialised before declaration
+
+
+
+
+
+// console.log(message);
+// var message = "Hello";    // output -- undefined
+
+
+
+
+// message = "sfds"
+// console.log(message)
+
+
+
+// let a = "abcd"
+// a = "abbd"
+// console.log(a)
+
+
+
+
+
+
+// console.log(typeof(age));
+// let age = 20;
+
+
+
+
+// let arr = ['a','b','c','d'];
+// console.log(arr);
+
+
+// let age = 12;
+// let s = "120";
+// console.log(s-age)
+
+
+
+
+// let a = "12";
+// let b = 20;
+// console.log(a*b);
+
+
+
+
+// let s = "12";
+// let p = parseInt(s);
+// let n = Number(s);
+
+
+
+
+
+// let a = 12;
+// let b;
+// b === a;
+
+
+
+// let a = "12";
+// let b = 12;
+
+// if("12"==b) console.log("1y")
+// else console.log("1n")
+
+// if("12"===b) console.log("2y")
+// else console.log("2n")
+
+
+
+
+
+// let a = [61,6,645,5345,54];
+
+// a.forEach(element => {
+//     console.log(element)
+// });
+
+// for(e in a){
+//     console.log(e)
+// }
+
+
+
+// function sum(a=0,b=0){
+//     console.log(a+b);
+// }
+
+// sum()
+// sum(2)
+// sum(2,3)
+
+
+
+
+// const a = function print(){console.log("---"); return 2}
+// a()
+// // print();
+// console.log(a());
+
+
+
+ 
+// let a = prompt("Enter name")
+// console.log()
+
+
+
+// console.log(null === undefined)
+
+
+
+// const obj = new Object();
+// console.log(obj)
+
+
+
+
+
+// const obj = new Object();
+// obj[12] = "Sparsh";
+// obj["name"] = "Ram";
+// obj[10] = 12;
+// const p = prompt()
+// console.log(obj[p])
